f8913ecda3e7203ec76fa0a85431ef819a04c5a5
Rafal Wojdyla <rav@spotify.com>
2014-06-22 20:49:04 +0200

    Compile new protos - devided by protocol types
    Compile common, hdfs, namenode and datanodes protocols.

--- scripts/compile_protos.sh
+++ scripts/compile_protos.sh
@@ -22,13 +22,7 @@ fi
 echo "Protobuf compiler (protoc) available - ${PROTOC_CMD} - version ${PROTOC_VERSION}"
 
 # Get proto files:
-PROTO_FILES_DIR="${MY_DIR}/../protos"
-PROTO_FILES="$( ls  ${PROTO_FILES_DIR}/*.proto )"
-
-echo "Proto files:"
-for file in ${PROTO_FILES}; do
-    echo -e "\t* $( basename $file )"
-done
+PROTOS_BASE_DIR="${MY_DIR}/../protos"
 
 # Compile protos
 PROTO_OUT_DIR="${MY_DIR}/../hbite/protos"
@@ -36,7 +30,25 @@ PROTO_OUT_DIR="${MY_DIR}/../hbite/protos"
 mkdir -p ${PROTO_OUT_DIR}
 touch ${PROTO_OUT_DIR}/__init__.py
 
-${PROTOC_CMD} --proto_path=${PROTO_FILES_DIR} --python_out=${PROTO_OUT_DIR} ${PROTO_FILES}
+HDFS_PROTOS="${PROTOS_BASE_DIR}/hdfs"
+COMMON_PROTOS="${PROTOS_BASE_DIR}/common"
+
+COMMON_PROTOCOL=$(ls ${COMMON_PROTOS}/*.proto)
+HDFS_PROTOCOL="${HDFS_PROTOS}/HAZKInfo.proto ${HDFS_PROTOS}/InterDatanodeProtocol.proto"\
+" ${HDFS_PROTOS}/JournalProtocol.proto ${HDFS_PROTOS}/datatransfer.proto ${HDFS_PROTOS}/hdfs.proto"
+DATANODE_PROTOCOL="${HDFS_PROTOS}/ClientDatanodeProtocol.proto ${HDFS_PROTOS}/DatanodeProtocol.proto"
+NAMENODE_PROTOCOL="${HDFS_PROTOS}/ClientNamenodeProtocol.proto ${HDFS_PROTOS}/NamenodeProtocol.proto"
+
+ALL_PROTOCOLS=("${COMMON_PROTOCOL}" "${HDFS_PROTOCOL}" "${DATANODE_PROTOCOL}" "${NAMENODE_PROTOCOL}")
+
+for protocol in "${ALL_PROTOCOLS[@]}"; do
+    echo "Proto files:"
+    for file in ${protocol}; do
+        echo -e "\t* $( basename $file )"
+    done
+
+    ${PROTOC_CMD} --proto_path=${COMMON_PROTOS} --proto_path=${HDFS_PROTOS} --python_out=${PROTO_OUT_DIR} ${protocol}
+done
 
 if [[ $? != 0 ]]; then
     echo "Proto files compilation failed!"
