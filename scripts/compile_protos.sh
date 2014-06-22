27f2e5596c459ff97f971ab384e8ac7b1e4aa53c
Rafal Wojdyla <rav@spotify.com>
2014-06-22 18:11:39 +0200

    Fix whitespaces/style in compile_protos script

--- scripts/compile_protos.sh
+++ scripts/compile_protos.sh
@@ -11,7 +11,7 @@ if [[ -z ${PROTOC_CMD} ]]; then
     exit -1
 fi
 
-PROTOC_VERSION="$(${PROTOC_CMD} --version | cut -f 2 -d ' ')"
+PROTOC_VERSION="$( ${PROTOC_CMD} --version | cut -f 2 -d ' ' )"
 PROTOC_VERSION_REQUIRED="2.5.0"
 
 if [[ ${PROTOC_VERSION} != ${PROTOC_VERSION_REQUIRED} ]]; then
@@ -23,11 +23,11 @@ echo "Protobuf compiler (protoc) available - ${PROTOC_CMD} - version ${PROTOC_VE
 
 # Get proto files:
 PROTO_FILES_DIR="${MY_DIR}/../protos"
-PROTO_FILES="$(ls  ${PROTO_FILES_DIR}/*.proto)"
+PROTO_FILES="$( ls  ${PROTO_FILES_DIR}/*.proto )"
 
 echo "Proto files:"
 for file in ${PROTO_FILES}; do
-    echo -e "\t* $(basename $file)"
+    echo -e "\t* $( basename $file )"
 done
 
 # Compile protos
@@ -43,7 +43,7 @@ if [[ $? != 0 ]]; then
     exit -1
 else
     echo "Success - proto files compiled to:"
-    for file in $(ls ${PROTO_OUT_DIR}); do
-        echo -e "\t* $(basename $file)"
+    for file in $( ls ${PROTO_OUT_DIR} ); do
+        echo -e "\t* $( basename $file )"
     done
 fi
